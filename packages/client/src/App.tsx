da9cca1355db2cd8f765fd65947ab1db585f2dfb
IvanB56 <burack.iw@yandex.ru>
2023-03-10 10:22:50 +0300

    [KUBIK-22] add router

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,4 @@
-import ForumPage from './pages/ForumPage/ForumPage'
 import AppRouter from './router/AppRouter'
-import LoginPage from './pages/LoginPage/LoginPage'
-import { Route } from 'react-router-dom'
-import LeaderBoardPage from './pages/LeaderBoardPage/LeaderBoardPage'
 import './App.scss'
 
 function App() {
