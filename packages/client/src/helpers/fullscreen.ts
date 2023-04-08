ed72e55ba9bc8a67fde8bb5e68f7ae919aa4dbcc
yulaivanova <yulialepehina@gmail.com>
2023-04-08 15:18:31 +0300

    Вносит правки по ревью

--- packages/client/src/helpers/fullscreen.ts
+++ packages/client/src/helpers/fullscreen.ts
@@ -1,7 +1,5 @@
 import { FullscreenDocument, FullscreenElement } from '../types/types';
 
-// const fullscreenDocument = document as FullscreenDocument;
-
 function enterFullscreen(element: FullscreenElement): void {
   if (element.requestFullscreen) {
     element.requestFullscreen();
@@ -15,23 +13,25 @@ function enterFullscreen(element: FullscreenElement): void {
 }
 
 function exitFullscreen(): void {
-  if ((document as FullscreenDocument).exitFullscreen) {
-    (document as FullscreenDocument).exitFullscreen();
-  } else if ((document as FullscreenDocument).mozCancelFullScreen) {
-    (document as FullscreenDocument).mozCancelFullScreen();
-  } else if ((document as FullscreenDocument).webkitExitFullscreen) {
-    (document as FullscreenDocument).webkitExitFullscreen();
-  } else if ((document as FullscreenDocument).msExitFullscreen) {
-    (document as FullscreenDocument).msExitFullscreen();
+  const fullscreenDocument = document as FullscreenDocument;
+  if (fullscreenDocument.exitFullscreen) {
+    fullscreenDocument.exitFullscreen();
+  } else if (fullscreenDocument.mozCancelFullScreen) {
+    fullscreenDocument.mozCancelFullScreen();
+  } else if (fullscreenDocument.webkitExitFullscreen) {
+    fullscreenDocument.webkitExitFullscreen();
+  } else if (fullscreenDocument.msExitFullscreen) {
+    fullscreenDocument.msExitFullscreen();
   }
 }
 
 function checkFullscreen(): Element | null {
+  const fullscreenDocument = document as FullscreenDocument;
   return (
-    (document as FullscreenDocument).fullscreenElement ||
-    (document as FullscreenDocument).mozFullScreenElement ||
-    (document as FullscreenDocument).webkitFullscreenElement ||
-    (document as FullscreenDocument).msFullscreenElement
+    fullscreenDocument.fullscreenElement ||
+    fullscreenDocument.mozFullScreenElement ||
+    fullscreenDocument.webkitFullscreenElement ||
+    fullscreenDocument.msFullscreenElement
   );
 }
 
--- packages/client/ssr.tsx
+++ packages/client/ssr.tsx
@@ -1,14 +1,16 @@
 import App from './src/App';
 import { renderToString } from 'react-dom/server';
 import { store } from './src/store/store';
-import { BrowserRouter } from 'react-router-dom';
+import { StaticRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import React from 'react';
 
 export function render() {
   return renderToString(
     <Provider store={store}>
-      <App />
+      <StaticRouter>
+        <App />
+      </StaticRouter>
     </Provider>
   );
 }
