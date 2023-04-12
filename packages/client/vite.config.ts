9b4f82b9372aa2339dffc19b4944e6631bedd3c5
IvanB56 <burack.iw@yandex.ru>
2023-04-12 12:55:07 +0300

    [KUBIK-27] fix bugs

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,28 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
-// import { rollup, InputOptions, OutputOptions } from 'rollup'
-// import rollupPluginTypescript from 'rollup-plugin-typescript'
-// import { nodeResolve } from '@rollup/plugin-node-resolve'
-// import { VitePWA } from 'vite-plugin-pwa'
+import { rollup, InputOptions, OutputOptions } from 'rollup'
+import rollupPluginTypescript from 'rollup-plugin-typescript'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import { VitePWA } from 'vite-plugin-pwa'
 dotenv.config()
 
-// const CompileTsServiceWorker = () => ({
-//   name: 'compile-typescript-service-worker',
-//   async writeBundle(_options, _outputBundle) {
-//     const inputOptions: InputOptions = {
-//       input: 'sw.js',
-//       plugins: [rollupPluginTypescript(), nodeResolve()],
-//     }
-//     const outputOptions: OutputOptions = {
-//       file: 'dist/sw.js',
-//       format: 'es',
-//     }
-//     const bundle = await rollup(inputOptions)
-//     await bundle.write(outputOptions)
-//     await bundle.close()
-//   },
-// })
+const CompileTsServiceWorker = () => ({
+  name: 'compile-typescript-service-worker',
+  async writeBundle(_options, _outputBundle) {
+    const inputOptions: InputOptions = {
+      input: 'sw.js',
+      plugins: [rollupPluginTypescript(), nodeResolve()],
+    }
+    const outputOptions: OutputOptions = {
+      file: 'dist/sw.js',
+      format: 'es',
+    }
+    const bundle = await rollup(inputOptions)
+    await bundle.write(outputOptions)
+    await bundle.close()
+  },
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -34,13 +34,13 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    // VitePWA({
-    //   registerType: 'autoUpdate',
-    //   workbox: {
-    //     importScripts: ['./sw-functional.js'],
-    //     globIgnores: ['**/node_modules/**/*', '**/sw.js'],
-    //   },
-    // }),
-    // CompileTsServiceWorker(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        importScripts: ['./sw.js'],
+        globIgnores: ['**/node_modules/**/*', '**/sw.js'],
+      },
+    }),
+    CompileTsServiceWorker(),
   ],
 })
