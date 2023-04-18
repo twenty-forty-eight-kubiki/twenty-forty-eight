563a831d01221bc25eb629af47bb30084112c929
yulaivanova <yulialepehina@gmail.com>
2023-04-18 19:13:00 +0300

    правит по ревью

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,17 +1,30 @@
 import AppRouter from './router/AppRouter';
 import { fetchUser } from './store/auth-actions';
 import { useAppDispatch, useAppSelector } from './hooks/store';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react'
 import { getAuthCheckedStatus } from './store/selectors';
 
 const App = () => {
   const dispatch = useAppDispatch();
+  const [domLoaded, setDomLoaded] = useState(false);
+
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
+
   useEffect(() => {
     dispatch(fetchUser());
   }, []);
   const isAuthChecked = useAppSelector(getAuthCheckedStatus);
 
-  return isAuthChecked ? <AppRouter /> : <p>Loading...</p>;
+  return <>
+    {domLoaded && (
+      isAuthChecked ? <AppRouter /> : <div>Loading...</div>
+    )}
+  </>
+
+
+
 };
 
 export default App;
