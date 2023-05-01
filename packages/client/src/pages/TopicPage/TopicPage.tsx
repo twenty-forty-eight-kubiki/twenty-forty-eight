25c1093467deb1337e5e43850f78343c2d913ee2
IvanB56 <burack.iw@yandex.ru>
2023-05-01 19:47:13 +0300

    KUBIK-58 send comments

--- packages/client/src/pages/TopicPage/TopicPage.tsx
+++ packages/client/src/pages/TopicPage/TopicPage.tsx
@@ -1,103 +1,103 @@
-import React, {FormEvent, useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom';
+import React, { FormEvent, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Topic from '../../components/Topic/Topic';
 import Comments from '../../components/Comments/Comments';
 import Loader from '../../components/Loader/Loader';
 import GuiInput from '../../ui/GuiInput/GuiInput';
 import GuiButton from '../../ui/GuiButton/GuiButton';
-import {useAppSelector} from '../../hooks/store';
-import {getUserData} from '../../store/selectors';
-import {withLayout} from '../../hocs/withLayout';
+import { useAppSelector } from '../../hooks/store';
+import { getUserData } from '../../store/selectors';
+import { withLayout } from '../../hocs/withLayout';
 import './TopicPage.scss';
 
 interface TopicPageData {
-    userId: number | null;
-    id: number | null;
-    title: string;
-    body: string;
+  userId: number | null;
+  id: number | null;
+  title: string;
+  body: string;
 }
 
 interface CommentsData {
-    postId: number | null;
-    id: number | null;
-    name: string;
-    email: string;
-    body: string;
+  postId: number | null;
+  id: number | null;
+  name: string;
+  email: string;
+  body: string;
 }
 
 const TopicPage = () => {
-    const initialTopic: TopicPageData = {
-        userId: null,
-        id: null,
-        title: '',
-        body: ''
-    };
-    const [domLoaded, setDomLoaded] = useState(false);
-    const [topic, setTopic] = useState<TopicPageData>(initialTopic);
-    const [comments, setComments] = useState<CommentsData[]>([]);
-    const [commentText, setCommentText] = useState('');
-    const params = useParams<{ postId: string }>();
-    const userData = useAppSelector(getUserData);
+  const initialTopic: TopicPageData = {
+    userId: null,
+    id: null,
+    title: '',
+    body: ''
+  };
+  const [domLoaded, setDomLoaded] = useState(false);
+  const [topic, setTopic] = useState<TopicPageData>(initialTopic);
+  const [comments, setComments] = useState<CommentsData[]>([]);
+  const [commentText, setCommentText] = useState('');
+  const params = useParams<{ postId: string }>();
+  const userData = useAppSelector(getUserData);
 
-    useEffect(() => {
-        topicFetch().then();
-        commentsFetch().then();
-        setDomLoaded(true);
-    }, []);
+  useEffect(() => {
+    topicFetch().then();
+    commentsFetch().then();
+    setDomLoaded(true);
+  }, []);
 
-    const topicFetch = async () => {
-        fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
-            .then(res => res.json())
-            .then(data => setTopic(data));
-    };
+  const topicFetch = async () => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+      .then(res => res.json())
+      .then(data => setTopic(data));
+  };
 
-    const commentsFetch = async () => {
-        fetch(
-            `https://jsonplaceholder.typicode.com/posts/${params.postId}/comments`
-        )
-            .then(res => res.json())
-            .then(data => setComments(data));
-    };
+  const commentsFetch = async () => {
+    fetch(
+      `https://jsonplaceholder.typicode.com/posts/${params.postId}/comments`
+    )
+      .then(res => res.json())
+      .then(data => setComments(data));
+  };
 
-    const onChangeComment = (e: FormEvent) => {
-        setCommentText((e.target as HTMLInputElement).value);
-    };
+  const onChangeComment = (e: FormEvent) => {
+    setCommentText((e.target as HTMLInputElement).value);
+  };
 
-    const commentSend = () => {
-        if (commentText.trim() !== '') {
-            const comment: CommentsData = {
-                body: commentText,
-                email: userData!.email,
-                id: Date.now(),
-                postId: userData!.id,
-                name: userData!.login
-            }
-            setComments([...comments, comment]);
-            setCommentText('');
-        }
-    };
+  const commentSend = () => {
+    if (commentText.trim() !== '') {
+      const comment = {
+        body: commentText,
+        email: userData!.email,
+        id: Date.now(),
+        postId: userData!.id,
+        name: userData!.login
+      } as CommentsData ?? {};
+      setComments([...comments, comment]);
+      setCommentText('');
+    }
+  };
 
-    return (
-        <>
-            {domLoaded ? (
-                <div className='topic-page'>
-                    <Topic topic={topic}/>
-                    <div className='send-comments'>
-                        <GuiInput
-                            value={commentText}
-                            label=''
-                            placeholder='Оставить комментарий'
-                            onChange={onChangeComment}
-                        />
-                        <GuiButton btnText='Отправить' onClick={commentSend}/>
-                    </div>
-                    <Comments comments={comments}/>
-                </div>
-            ) : (
-                <Loader/>
-            )}
-        </>
-    );
+  return (
+    <>
+      {domLoaded ? (
+        <div className='topic-page'>
+          <Topic topic={topic} />
+          <div className='send-comments'>
+            <GuiInput
+              value={commentText}
+              label=''
+              placeholder='Оставить комментарий'
+              onChange={onChangeComment}
+            />
+            <GuiButton btnText='Отправить' onClick={commentSend} />
+          </div>
+          <Comments comments={comments} />
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </>
+  );
 };
 
 export default withLayout(TopicPage);
